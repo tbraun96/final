9275ac9649a889a9ca91ccfdfaf9ee32df4793b1
Thomas Braun <tbraun96@gmail.com>
2019-06-05 09:16:36 -0700

    first

---
+++ README.md
@@ -0,0 +1,8 @@
+# pncreek
+
+
+the converter cmd
+
+```
+PotreeConverter.exe assets/merge.las -o web  --projection "+proj=utm +zone=10 +ellps=GRS80 +datum=NAD83 +units=m +no_defs" -o web -p index
+```
---
+++ cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "+proj=utm +zone=10 +ellps=GRS80 +datum=NAD83 +units=m +no_defs",
+    "points": 3920538,
+    "boundingBox": {
+        "lx": 476828.47,
+        "ly": 4942195.899999999,
+        "lz": 193.8,
+        "ux": 477660.54,
+        "uy": 4943027.97,
+        "uz": 1025.870000000007
+    },
+    "tightBoundingBox": {
+        "lx": 476828.47,
+        "ly": 4942195.899999999,
+        "lz": 193.8,
+        "ux": 477660.54,
+        "uy": 4942902.579999999,
+        "uz": 400.43
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 7.205937385559082,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}

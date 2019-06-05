819cb23771193b2b5812f3323a9d037a2fed4b04
Thomas Braun <tbraun96@gmail.com>
2019-06-05 12:04:42 -0700

    fixes

---
+++ libs/geotiff/compression/basedecoder.js
@@ -0,0 +1,19 @@
+import { applyPredictor } from '../predictor';
+
+export default class BaseDecoder {
+  decode(fileDirectory, buffer) {
+    const decoded = this.decodeBlock(buffer);
+    const predictor = fileDirectory.Predictor || 1;
+    if (predictor !== 1) {
+      const isTiled = !fileDirectory.StripOffsets;
+      const tileWidth = isTiled ? fileDirectory.TileWidth : fileDirectory.ImageWidth;
+      const tileHeight = isTiled ? fileDirectory.TileLength : (
+        fileDirectory.RowsPerStrip || fileDirectory.ImageLength
+      );
+      return applyPredictor(
+        decoded, predictor, tileWidth, tileHeight, fileDirectory.BitsPerSample,
+      );
+    }
+    return decoded;
+  }
+}
